07eab0c97338426680b1ac66818b370ea82feec3
Vincent Langat <kimutailangat033@gmail.com>
2022-06-06 12:59:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = "Welcome to Flatbook,";
+
+ let welcomeMessage = currentUser;
+  welcomeMessage = "Welcome to Flatbook, ";
+  welcomeMessage = "Welcome to Flatbook, !";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+excitedWelcomeMessage = currentUser;
+excitedWelcomeMessage = "WELCOME TO FLATBOOK, !";
+let shortGreeting = "Welcome, "
+ shortGreeting ="Welcome, !"
